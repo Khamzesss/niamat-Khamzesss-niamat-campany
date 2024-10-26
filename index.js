a9851cb6c7ffa19c7eb8456527368a5a580a0573
Khamzesss <zhamzat00700795@gmail.com>
2024-10-26 22:00:17 +0300

    niamatt

---
+++ index.js
@@ -0,0 +1,34 @@
+const correctAnswers = ["114", "Адам (1алейх1и-с-салям)", "Сура Ар-Каусар" ,"Сура Аль-Бакъара", "В пещере Хира"];
+
+// function checkAnswer(button) {
+// const selectedAnswer = button.textContent;
+// if (selectedAnswer === "Не знаю, но хочу знать.") {
+// button.style.backgroundColor = "#0076fe";
+// } else if (correctAnswers.includes(selectedAnswer)) {
+// button.style.backgroundColor = "green";
+// } else {
+// button.style.backgroundColor = "red";
+// }
+// }
+
+function checkAnswer(button) {
+  const selectedAnswer = button.textContent;
+  if (selectedAnswer === "Не знаю, но хочу знать.") {
+  const correctAnswerButton = Array.from(button.parentElement.children).find(child => correctAnswers.includes(child.textContent));
+  correctAnswerButton.style.backgroundColor = "green";
+  } else if (correctAnswers.includes(selectedAnswer)) {
+  button.style.backgroundColor = "green";
+  } else {
+  button.style.backgroundColor = "red";
+  }
+  }
+
+
+// Mobile adaptation
+const options = document.querySelectorAll(".option");
+options.forEach((option) => {
+  option.addEventListener("click", function () {
+    option.style.padding = "20px 30px";
+  });
+});
+//     /* Если ты зашел сюда и решил взять код, то я дарю его тебе. Хьанал хуьйла! */
